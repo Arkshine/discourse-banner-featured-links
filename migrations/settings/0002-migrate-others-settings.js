0f2ab6af12c9e13d04fb8d5a07ca91e75f0592ae
Arkshine <github@arkshine.me>
2024-09-21 00:28:30 +0200

    DEV: Remove debug

--- migrations/settings/0002-migrate-others-settings.js
+++ migrations/settings/0002-migrate-others-settings.js
@@ -3,7 +3,6 @@ const migrateSetting = (settings, oldKey, newKey, transform = (v) => v) => {
     settings.set(newKey, transform(settings.get(oldKey)));
     settings.delete(oldKey);
   }
-  console.log("settings", settings, Object.fromEntries(settings.entries()));
   return settings;
 };
 
@@ -17,19 +16,22 @@ export default function migrate(settings) {
     (value) => `inset 0 0 0 2px ${value}`
   );
 
-  settings.set(
-    "links",
-    settings.get("links").map((link) => {
-      return Object.fromEntries(
-        migrateSetting(
-          new Map(Object.entries(link)),
-          "button_shadow_color",
-          "button_shadow",
-          (value) => `inset 0 0 0 2px ${value}`
-        ).entries()
-      );
-    })
-  );
+  const links = settings.get("links");
+  if (links) {
+    settings.set(
+      "links",
+      links.map((link) => {
+        return Object.fromEntries(
+          migrateSetting(
+            new Map(Object.entries(link)),
+            "button_shadow_color",
+            "button_shadow",
+            (value) => `inset 0 0 0 2px ${value}`
+          ).entries()
+        );
+      })
+    );
+  }
 
   return settings;
 }
